3ee40364f7be76f38cd047b9cc7c8595d0839401
parkaway <locked.ani@gmail.com>
2020-03-29 16:15:43 +0530

    Just an useless update

---
+++ get.php
@@ -0,0 +1,15 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+ 
+ // Try Connect to the DB with new MySqli object - Params {hostname, userid, password, dbname}
+ $mysqli = new mysqli("localhost", "root", "", "boredaf");
+
+            
+ $statement = $mysqli->prepare("SELECT * FROM tasks ORDER BY RAND() LIMIT 1");
+
+
+ $statement->execute(); // Execute the statement.
+ $result = $statement->get_result(); // Binds the last executed statement as a result.
+
+ echo json_encode(($result->fetch_assoc())); // Parse to JSON and print.
+?>
